d62df270018bd56f69091744bf1aadd3500bb131
Rafael Santos <rafael.santos.193@gmail.com>
2020-11-22 10:39:04 -0300

    feat: Keyboard cursor working

--- src/App.js
+++ src/App.js
@@ -61,27 +61,79 @@ const useTimer = () => {
   ]
 }
 
+const KEY_CODE_ARROW_RIGHT = 39
+const KEY_CODE_ARROW_LEFT = 37
+const KEY_CODE_SPACE = 32
+const KEY_CODE_ENTER = 13
+
 const MyCards = () => {
+  const [ cardCount, setCardCount ] = useState(1)
+  const [ focusIndex, setFocusIndex ] = useState(0)
+
+  const addCard = () => {
+    setCardCount(cardCount + 1)
+  }
+
+  useEffect(() => {
+    const focusRight = () => setFocusIndex(focusIndex+1)
+    const focusLeft = () => setFocusIndex(focusIndex-1)
+    const stopwatchToggle = () => {
+      
+    }
+    const addStopwatch = () => addCard()
+    const handleKeyDown = (e) => {
+      const k = e.keyCode
+      if (k === KEY_CODE_ARROW_RIGHT) focusRight()
+      if (k === KEY_CODE_ARROW_LEFT) focusLeft()
+      if (k === KEY_CODE_SPACE) stopwatchToggle()
+      if (k === KEY_CODE_ENTER) addStopwatch()
+    }
+    document.addEventListener("keydown", handleKeyDown, true)
+    return () => document.removeEventListener("keydown", handleKeyDown, true)
+  }, [cardCount, focusIndex])
+
+  let cards = []
+
+  for (let i = 0; i < cardCount; i++) {
+    let hasFocus = false
+    if (i === focusIndex) {
+      hasFocus = true
+    }
+    cards.push(<MyCardCol key={i} hasFocus={hasFocus}/>)
+  }
+
   return (
     <rs.Row>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
-      <MyCardCol/>
+      {cards}
+      <AddCard addCard={addCard}/>
     </rs.Row>
   )
 }
 
-const MyCardCol = () => {
+const MyCardCol = (props) => {
+  const [ isHidden, setIsHidden ] = useState(false)
+  if (isHidden) {
+    return <></>
+  }
+
+  let style = {}
+  if (props.hasFocus) {
+    style["borderColor"] = "black"
+  }
+
   return (
     <rs.Col md="4" style={{marginTop: 20}}>
-      <rs.Card>
-        <MyCardBody/>
+      <rs.Card style={style}>
+        <MyCardBody handleHide={() => setIsHidden(true)}/>
       </rs.Card>
+    </rs.Col>
+  )
+}
+
+const AddCard = (props) => {
+  return (
+    <rs.Col md="4" sml="12" style={{marginTop: 20}}>
+      <rs.Button color="secondary" style={{fontSize: "200%"}} onClick={props.addCard}>+</rs.Button>
     </rs.Col>
   )
 }
@@ -95,21 +147,39 @@ const useForm = (placeholder) => {
   }
 }
 
-const MyCardBody = () => {
-  const [ count, formattedCount, isCounting, toggleCounter, restartCounter ] = useTimer()
+const DeleteButton = (props) => {
   return (
-    <rs.CardBody style={ isCounting ? {background: "#e6ffeb"} : (count !== 0) ? {background: "#fffaeb"} : {} } >
-      <Title/>
+    <rs.Button onClick={props.handleClick} color="danger" style={{float: "right", fontSize: "100%"}}>x</rs.Button>
+  )
+}
+
+const MyCardBody = (props) => {
+  const [ count, formattedCount, isCounting, toggleCounter, restartCounter ] = useTimer()
+
+  let style = {}
+
+  if (isCounting) {
+    style["background"] = "#e6ffeb"
+  } else if (count !== 0) {
+    style["background"] = "#fffaeb"
+  }
+
+  return (
+    <rs.CardBody style={style}>
+      <div style={{zIndex: 1}}>
+        <Title/>
+      </div>
       <rs.CardText>Seconds ellapsed: {formattedCount}</rs.CardText>
       <rs.Button color="primary" style={{marginRight: 5}} onClick={toggleCounter}>{ isCounting ? "Pause" : (count === 0) ? "Start" : "Continue" }</rs.Button>
-      { (count !== 0) ? <rs.Button onClick={restartCounter}>Reset</rs.Button> : "" }
+      <DeleteButton handleClick={props.handleHide}/>
+      { (count !== 0) ? <rs.Button color="danger" style={{marginRight: 5, float: "right"}} onClick={restartCounter}>Reset</rs.Button> : "" }
     </rs.CardBody>
   )
 }
 
 const Title = () => {
   const formDescription = useForm("Enter a new title")
-  const [ isEditing, editButton, handleFinishEditting ] = useEditButton()
+  const [ isEditing, setIsEditing ] = useState(false)
 
   const ref = useRef(null);
 
@@ -120,42 +190,27 @@ const Title = () => {
     }
   }, [isEditing])
 
+  const handleTitleClick = (e) => {
+    setIsEditing(true)
+  }
+
+  const handleFormSubmit = (e) => {
+    setIsEditing(false)
+    e.preventDefault()
+  }
+
   return (
     <>
-    <rs.CardTitle tag="h5">
-      <form onSubmit={handleFinishEditting}>
+      <form onSubmit={handleFormSubmit}>
         { isEditing ? (
-          <rs.Input {...formDescription} style={{display: "inline", width: "auto"}} innerRef={ref}/> 
+          <rs.Input {...formDescription} onBlur={handleFormSubmit} innerRef={ref} style={{marginBottom: 5}}/>
         ) : (
-          <>{ formDescription.value || "Stopwatch" }</>
+          <rs.CardTitle tag="h5" onClick={handleTitleClick} style={{cursor: "pointer"}}>
+           { formDescription.value || "Stopwatch" }
+          </rs.CardTitle>
         )}
-        {editButton}
       </form>
-    </rs.CardTitle>
     </>
-  )
-}
-
-const useEditButton = () => {
-  const [ isEditing, setIsEditing ] = useState(false)
-  return [
-    isEditing,
-    <EditTitleButton setIsEditing={setIsEditing} isEditing={isEditing}/>,
-    (e) => {
-      setIsEditing(false)
-      e.preventDefault()
-    }
-  ]
-}
-
-const EditTitleButton = (props) => {
-  if (!props.isEditing) {
-    return (
-      <rs.Button size="sm" style={{marginLeft: 5}} onClick={() => props.setIsEditing(true)} >Set title</rs.Button>
-    )
-  }
-  return (
-    <rs.Button size="sm" style={{marginLeft: 5}} onClick={() => props.setIsEditing(false)} >OK</rs.Button>
   )
 }
 
